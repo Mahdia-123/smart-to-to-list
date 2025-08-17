5370d126e15532b44ef3356cb9df21e99c485942
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-08-17 11:32:03 +0430

    added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ export default function App() {
     <div className="App">
       <h1>My To-Do List</h1>
       <ToDoList />
+      <footer>
+        {" "}
+        <p>
+          This page was coded by <a href="">Mahdia Khamoosh</a>and is{" "}
+          <a href="">Open-sourced</a>and hosted on <a href="">Netlify</a>
+        </p>
+      </footer>
     </div>
   );
 }
